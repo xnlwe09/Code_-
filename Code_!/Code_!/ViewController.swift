7727808a8a98716ccd79d2b3d2ac7c8396262032
Juhui <joohee090331@gmail.com>
2025-11-03 21:52:45 +0900

    setting :: 초기 세팅

---
+++ Code_!/Code_!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Code_!
+//
+//  Created by 신주희 on 11/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
